8b646eea557bff2a2dd34bb530f1189612c7fa4d
muzhi@baofoo.com <muzhi@baofoo.com>
2019-07-03 15:07:13 +0800

    cloc

--- js/demo.js
+++ js/demo.js
@@ -89,7 +89,7 @@ function runTime() {
 function initStyle() {
     var label = document.getElementsByClassName('label');
     for (var i = 0; i < label.length; i++) {
-        label[i].style.color = '#4d4d4d';
+        label[i].style.color = '#f6e66a';
     }
 }
 function changePosition() {
